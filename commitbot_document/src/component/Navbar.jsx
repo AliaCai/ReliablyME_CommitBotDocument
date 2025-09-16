586124588b1c37dcbc91b06aa1e3db4e9d9bd215
AliaCai <Caiheyuncoco@outlook.com>
2025-09-16 19:26:35 -0400

    feature/alia-add-a-small-hoverable-box

--- commitbot_document/src/component/Navbar.jsx
+++ commitbot_document/src/component/Navbar.jsx
@@ -8,10 +8,8 @@ const Navbar = () => {
         <div className="navBar_logo"><img className="navBar_logoImg" src={company_logo} alt="reliablyME" /></div>
 
         <div className="gpt">
-          <a href='https://chatgpt.com/g/g-68c34978bd188191954ec0bdf4d74850-reliablyme-commitbot-guide' target='_blank'> <div className="gpt_logo"><img className="gpt_logoImg" src={gpt_logo} alt="gpt logo" />
-          </div></a>
-
-          {/* <div className="gpt_describe">Feel free to click the button if you have more questions.</div> */}
+          <a href='https://chatgpt.com/g/g-68c34978bd188191954ec0bdf4d74850-reliablyme-commitbot-guide' className="gpt_link" target='_blank'> <div className="gpt_logo"><img className="gpt_logoImg" src={gpt_logo} alt="gpt logo" /></div></a>
+          <div className="gpt_describe">Feel free to click the button if you have more questions.</div>
         </div>
 
         {/* <div className="navBar_search">
